adb26207277ce1445eaeb8ebd251f1c11778f7b6
Chloe Williams <chloe.williams@and.digital>
2020-06-16 12:33:34 +0100

    implements fetch for users within given radius of city

--- models/users-model.js
+++ models/users-model.js
@@ -1,10 +1,29 @@
-const axios = require('axios');
+const axios = require("axios");
+const geolib = require("geolib");
 
 const baseUrl = "https://bpdts-test-app.herokuapp.com";
 
-const fetchUsersByCity = async (city) => {
-    const { data } = await axios.get(`${baseUrl}/city/${city}/users`)
-    return data;
-}
+const milesConversion = miles => {
+  return miles * 1609;
+};
 
-module.exports = { fetchUsersByCity }
+const checkDistance = (user, meters) => {
+  return geolib.isPointWithinRadius(
+    { latitude: user.latitude, longitude: user.longitude },
+    { latitude: 51.50853, longitude: -0.12574 },
+    meters
+  );
+};
+
+const fetchUsersByCity = async city => {
+  const { data } = await axios.get(`${baseUrl}/city/${city}/users`);
+  return data;
+};
+
+const fetchUsersWithinRadius = async miles => {
+  const meters = milesConversion(miles);
+  const { data } = await axios.get(`${baseUrl}/users`);
+  return data.filter(user => checkDistance(user, meters));
+};
+
+module.exports = { fetchUsersByCity, fetchUsersWithinRadius };
--- spec/app.spec.js
+++ spec/app.spec.js
@@ -12,13 +12,17 @@ describe("/users/:city", () => {
     nock(baseUrl)
       .get("/city/London/users")
       .reply(200, [...usersByCity]);
+
+    nock(baseUrl)
+      .get("/users")
+      .reply(200, [...allUsers]);
   });
 
   afterEach(() => {
     nock.cleanAll();
   });
 
-  it("GET responses with status: 200 and an array of users listed as living in London", () => {
+  it("GET responds with status: 200 and an array of users listed as living in London", () => {
     return request
       .get("/users/London")
       .expect(200)
@@ -26,6 +30,14 @@ describe("/users/:city", () => {
         expect(users).to.be.an("array");
         expect(users.length).to.equal(4);
         expect(users[0]).to.contain.keys("email", "first_name", "latitude");
+      });
+  });
+  it("GET responds with status: 200 and an array of users who are listed as either living in London, or whose current coordinates are within 50 miles of London", () => {
+    return request
+      .get("/users/London?miles=50")
+      .expect(200)
+      .then(({ body: { users } }) => {
+        expect(users.length).to.equal(6);
       });
   });
 });
